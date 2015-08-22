85c9356fc9f02704db2edb76552bc30ece492dc0
TonyHsu <sunprince12014@gmail.com>
2015-08-22 21:34:18 +0800

    new structure added

--- mysite/trips/models.py
+++ mysite/trips/models.py
@@ -1,12 +1,25 @@
 from django.db import models
+from django import forms
 
-# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True)
     photo = models.URLField(blank=True)
+    required_time = models.IntegerField()
     location = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.title
+
+class Block(models.Model):
+    post = models.ForeignKey(Post)
+    comment = models.TextField()
+    photo = models.URLField()
+    location = models.CharField(max_length=100)
+    rating = models.DecimalField(max_digits=1, decimal_places=1)
+
+class ContactForm(forms.Form):
+    subject=forms.CharField(max_length=100)
+    message=forms.CharField()
+    sender=forms.EmailField()
+    cc_myself=forms.BooleanField(required=False)
+# Create your models here.
